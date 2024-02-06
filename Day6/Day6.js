9fd5de3460a55b4cd384836e7db5a8b0eab9c7a1
DEVDEEP SINGHA <devdeepkonsam1212@gmail.com>
2024-02-06 18:07:13 +0530

    day6 commit

---
+++ Day6/Day6.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+/**
+ * Handles GET requests to "/greet" endpoint
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
+function greetHandler(req, res) {
+    // Your implementation here
+    const name = req.query.name;
+    console.log(name)
+    if (name) {
+        res.send(`Hello, ${name}!`);
+    } else {
+        res.send('Hello, Guest!');
+    }
+}
+
+app.get('/greet', greetHandler);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+
+
+app.get('/greet?name=John', greetHandler);
